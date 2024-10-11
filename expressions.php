3e32e2a0a068024c426cc41b14542ccd1611dbe4
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-10-11 16:02:00 +0800

    php pt.3 conditional

--- expressions.php
+++ expressions.php
@@ -3,12 +3,12 @@
     $name = "Noor";
     $pizza = 8;
     $pizzaAte = 2;
-    $pizzaRemaining = $pizza - $pizzaAte;
+    $pizzaRemaining = $pizza - $pizzaAte; //type coercion
     $hungry = false;
 
     echo "Hello World\n";
     echo "Welcome " . $name . " to PHP\n";
-    echo "This is the remaining pizza " . $pizzaRemaining . "\n"; //type coercion
+    echo "This is the remaining pizza " . $pizzaRemaining . "\n"; 
     echo "Still hungry? ";
     echo $hungry ? "Yes" : "No"; //ternary operator
 ?>
