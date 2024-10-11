51eafae1ed7c9686f9763cf6d63f6c8522514fb2
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-10-11 15:08:33 +0800

    php pt.1 expression

--- expressions.php
+++ expressions.php
@@ -1,5 +1,14 @@
 <?php
 
-    echo "Hello World\n";
+    $name = "Noor";
+    $pizza = 8;
+    $pizzaAte = 2;
+    $pizzaRemaining = $pizza - $pizzaAte;
+    $hungry = false;
 
+    echo "Hello World\n";
+    echo "Welcome " . $name . " to PHP\n";
+    echo "This is the remaining pizza " . $pizzaRemaining . "\n"; //type coercion
+    echo "Still hungry? ";
+    echo $hungry ? "Yes" : "No"; //ternary operator
 ?>
