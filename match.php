f08028bddd8045f2ed3c84ba0313493ba78767d8
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-10-12 12:02:31 +0800

    php pt.11 functions

--- match.php
+++ match.php
@@ -2,7 +2,7 @@
 
     $status = 404;
 
-    $message = match ($status) {
+    $message = match ($status) { //uses strict comparison
         200, 300 => 'Success',
         400, 404, 500 => 'Error',
         default => 'Unknown'
