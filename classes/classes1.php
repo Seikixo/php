6bb24d1bc4685cb6b8f9b8f3a6aac6a42da897be
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-28 13:33:32 +0800

    Object Exercise 1

--- classes/classes1.php
+++ classes/classes1.php
@@ -1,6 +1,6 @@
 <?php
 
-    class Person{
+    /*class Person{
 
         public function __construct(public string $name, public int $age){ //The constructor method (__construct) is a special method that gets called automatically when an object of the class is instantiated.
         }
@@ -8,7 +8,20 @@
         public function introduce(): string{ //method
             return "Hi my name is {$this->name} and my age is {$this->age} years old";
         }
+    }*/
+
+    class Client{
+        private $name;
+
+        public function setName($name){
+            $this->name = $name;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
     }
 
-    $person = new Person("Seikizo", 23); //A new object Person is created using the new keyword.
-    echo $person->introduce();
+    $employee = new Client();
+    $employee->setName("Vaughn");
+    echo($employee->getName());
